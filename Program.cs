29de46ff0618ee5fece079bb10c3c7f61007f5e5
Alexandr Osipov <ossipov44rus@gmail.com>
2022-07-25 19:58:13 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using Trees;
+
+List<Tree> myTreeColl = new List<Tree>();
+var a1 = new Tree { Name = "Osina", Children = myTreeColl };
+var a2 = new Tree { Name = "Dub" };
+myTreeColl.Add(a2);
+List<Tree> myTreeColl2 = new List<Tree>();
+var b2 = new Tree { Name = "Topol", Children = myTreeColl2 };
+myTreeColl.Add(b2);
+var a3 = new Tree { Name = "Yasen" };
+var b3 = new Tree { Name = "Bereza" };
+myTreeColl2.Add(a3);
+myTreeColl2.Add(b3);
+a1.WriteAllNamesInATree();
+
---
+++ Tree.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trees
+{
+    public class Tree
+
+    {
+        public string Name { get; set; }
+        public IEnumerable<Tree> Children { get; set; }
+        public void WriteAllNamesInATree()
+        {
+            Queue<Tree> myQueue = new Queue<Tree>();
+            myQueue.Enqueue(this);
+            while (myQueue.Count != 0)
+            {
+                var currentTree = myQueue.Peek();
+                Console.WriteLine(currentTree.Name);
+                if (currentTree.Children == null)
+                {
+                    myQueue.Dequeue();
+                }
+                else
+                {
+                    foreach (var j in currentTree.Children)
+                    {
+                        myQueue.Enqueue(j);
+                    }
+                    myQueue.Dequeue();
+                }
+            }
+        }
+
+    }
+}
